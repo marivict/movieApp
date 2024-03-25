851565e0082b120fb1031e749a2fbf63edd5bff8
marivict <mvdelarosaramirez@gmail.com>
2024-03-25 18:06:42 -0400

    feat: working on sidebar menu links

--- src/components/sidebar/SidebarBody/SidebarBody.tsx
+++ src/components/sidebar/SidebarBody/SidebarBody.tsx
@@ -1,11 +1,15 @@
 import { Container } from "@mui/material";
 import style from "./style.module.css";
+import { SidebarMenu } from "..";
+import MovieIcon from '@mui/icons-material/Movie';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
 export type SidebarBodyProps = {
     testId:string;
 }
 
 export const SidebarBody = ({testId}:SidebarBodyProps) => {
-    return(<Container className={`${style.SidebarBody}`}>
-            
+    return(<Container className={`${style.SidebarBody}`} disableGutters>
+            <SidebarMenu testId={testId} title={"Movies"} icon={<MovieIcon className={"sidebarMenu__icon"}/>} active/>
+            <SidebarMenu testId={testId} title={"Watchlist"} icon={<LiveTvIcon className={"sidebarMenu__icon"}/>} />
     </Container>)
 }
--- src/components/sidebar/SidebarMenu/SidebarMenu.tsx
+++ src/components/sidebar/SidebarMenu/SidebarMenu.tsx
@@ -1,6 +1,21 @@
+import { Link, Typography, Box } from "@mui/material";
+import style from "./style.module.css";
 export type SidebarMenuProps = {
     testId: string;
+    title: string;
+    icon?: React.ReactNode;
+    active?: boolean;
 }
-export const SidebarMenu = ({testId}:SidebarMenuProps) => {
-    return(<></>)
+export const SidebarMenu = ({testId, title, icon, active}:SidebarMenuProps) => {
+    return(
+    <Box className={`${style.sidebarMenu} ${icon ? style.sidebarMenu__icon__container: ""} ${active ? style.active : ""}`}>
+        {!!icon && icon}
+        <Link
+        data-testid = {testId}
+        className={`${style.sidebarMenu__link}`}
+        underline="none"
+>{title}
+    </Link>
+    </Box>
+    )
 }
