4c76242d909f9f66e4db4228e1d36d6af6ecc721
marivict <mvdelarosaramirez@gmail.com>
2024-10-15 17:04:22 -0400

    feat: search bar onchange

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import { InputBase } from "@mui/material";
 import style from "./searchBar.module.css";
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
 
 export const SearchBar = () => {
+    const [value, setValue] = useState("")
+    console.log(value);
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    }
+
     return <div className={style.searchBar__container}>
         <InputBase 
             className={style.searchBar}
@@ -12,6 +22,8 @@ export const SearchBar = () => {
             sx={{
                 color:"#fff",
             }}
+            value={value}
+            onChange={onChange}
         />
     </div>;
 }
